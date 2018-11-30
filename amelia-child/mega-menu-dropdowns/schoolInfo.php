b6dcbbef5287e02e578b3f14ce30af382d068e37
Josh Espinoza <joshespi@gmail.com>
2018-11-30 13:52:04 -0700

    Initial Upload

---
+++ amelia-child/mega-menu-dropdowns/schoolInfo.php
@@ -0,0 +1,37 @@
+<div class="dropDownColumn">
+	<h3><img src="https://timpviewbeta.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/calendars.svg" alt="" />Calendars</h3>
+		<ul>
+			<li class="int"><a href="https://sandbox2.provo.edu/amelia/all-events-calendar/">All Events Calendar</a></li>
+			<li class="int"><a href="https://sandbox2.provo.edu/amelia/calendar/">Amelia Earhart Calendar</a></li>
+			<li class="int"><a href="https://sandbox2.provo.edu/amelia/district-school-year-calendar/">School Year Calendar</a></li>
+					
+		</ul>
+</div>
+<div class="dropDownColumn">
+	<h3><img src="https://timpviewbeta.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/school-info.svg" alt="" />About Amelia Earhart</h3>
+		<ul>
+			<li class="int"><a href="https://sandbox2.provo.edu/amelia/school-information/contact-our-school/">Contact Our School</a></li>
+			<li class="int"><a href="https://sandbox2.provo.edu/amelia/school-information/school-profile/">School Profile</a></li>
+            <li class="int"><a href="https://sandbox2.provo.edu/amelia/school-information/school-hours/">Hours of Operation &amp; Daily Schedule</a></li>
+            <li class="pdf"><a href="https://sandbox2.provo.edu/amelia/wp-content/uploads/2018/11/Amelia_Earhart-Achievement-Data.pdf">Achievement Data</a></li>
+            <li class="int"><a href="https://sandbox2.provo.edu/amelia/school-information/community-council/">Community Council</a></li>
+            <li class="int"><a href="https://provo.edu/transportation/find-your-bus-route/">Bus Routes</a></li>
+            <li class="int"><a href="https://sandbox2.provo.edu/amelia/school-information/parent-teacher-association/">PTA</a></li>
+			<li class="int"><a href="https://sandbox2.provo.edu/amelia/school-information/volunteer-opportunities/">Volunteer Opportunities</a></li>
+			<li class="int"><a href="https://sandbox2.provo.edu/amelia/school-information/walking-to-school/">Walking to School</a></li>
+			<li class="pdf"><a href="https://sandbox2.provo.edu/amelia/wp-content/uploads/2018/11/Wasatch-Boundaries.pdf">Boundary Map</a></li>
+			<li class="int"><a href="https://sandbox2.provo.edu/amelia/school-information/before-after-school-program/">Before &amp; After School Program</a></li>
+			<li class="int"><a href="https://sandbox2.provo.edu/amelia/school-information/stem/">STEM Fair</a></li>
+		</ul>
+</div>
+<div class="dropDownColumn">
+	<h3><img src="https://timpviewbeta.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/school-meals.svg" alt="" />School Meals</h3>
+		<ul>
+			<li class="ext"><a href="https://www.myschoolbucks.com/ver2/login/getmain?requestAction=home">Pay for School Meals</a></li>
+            <li class="ext"><a href="https://www.schoollunchapp.com/Terms.aspx?host=provo.schoollunchapp.com&apply=1">Apply for Free and Reduced Meals</a></li>
+            <li class="ext"><a href="http://itsmealsprovo.com/?page=menus">School Menus</a></li>
+            <li class="int"><a href="https://provo.edu/child-nutrition/special-diet-accommodations/">Special Diet Accommodations</a></li>
+            <li class="int"><a href="https://provo.edu/child-nutrition/">Provo Child Nutrition Website</a></li>
+		</ul>
+	
+</div>
---
+++ amelia-child/mega-menu-dropdowns/titlei.php
@@ -0,0 +1,28 @@
+				<div class="dropDownColumn">
+					<h3>Title I Information</h3>
+						<ul>
+								<li class="pdf"><a href="https://docs.google.com/forms/d/e/1FAIpQLSfCe48I8zIDfz6obN6UIXxZ0OccCEeSiCmsyCwDNIwdnqQd-A/viewform?embedded=true">Title I Parent Info Questionnaire</a></li>
+								<li class="int"><a href="https://sandbox2.provo.edu/amelia/title-i/what-are-the-goals-of-title-i/">What are the Goals of Title I?</a></li>
+								<li class="pdf"><a href="https://sandbox2.provo.edu/amelia/wp-content/uploads/2018/11/Amelia-Earhart-FY19-School-Improvment-Plan.pdf">Title I School Improvement Plan</a></li>
+								<li>Title I Coordinator: Jonathan Hoehne (JonathanH@provo.edu)</li>
+						</ul>
+				</div>
+				<div class="dropDownColumn">
+					<h3>Parent Involvement Policy</h3>
+						<ul>
+							<li class="pdf"><a href="https://sandbox2.provo.edu/amelia/wp-content/uploads/2018/11/2017_18_ParentInvolvementPolicy.pdf">Parent Involvement Policy (English)</a></li>
+							<li class="pdf"><a href="https://sandbox2.provo.edu/amelia/wp-content/uploads/2018/11/2017_18_ParentInvolvementPolicySpanish.pdf">Parent Involvement Policy (Spanish)</a></li>
+						</ul>
+					<h3>Parent-School Compact</h3>
+						<ul>
+							<li class="pdf"><a href="https://sandbox2.provo.edu/amelia/wp-content/uploads/2018/11/2017_ParentSchoolCompact.pdf">Parent-School Compact (English)</a></li>
+							<li class="pdf"><a href="https://sandbox2.provo.edu/amelia/wp-content/uploads/2018/11/2017ParentSchoolCompactSpanish.pdf">Parent-School Compact (Spanish)</a></li>
+						</ul>
+				</div>
+				<div class="dropDownColumn">
+					<h3>Additional Resources</h3>
+						<ul>
+							<li class="pdf"><a href="https://sandbox2.provo.edu/amelia/wp-content/uploads/2018/11/Title-I-ComplaintProcedure.pdf">Title I Complaint Procedures</a></li>
+							<li class="pdf"><a href="https://sandbox2.provo.edu/amelia/wp-content/uploads/2018/11/Title-I-ComplaintForm.pdf">Title I Complaint Form</a></li>
+						</ul>
+				</div>
