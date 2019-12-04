a6cfb103c2d01c6e41e44090e4c4953799c44de8
Josh Espinoza <joshespi@gmial.com>
2019-12-04 10:53:06 -0700

    added SEP scheduler

--- amelia-child/sidebar.php
+++ amelia-child/sidebar.php
@@ -73,7 +73,12 @@
 							<span>Volunteer Opportunities</span>
 						</a>
 					</li>					
-					
+					<li>
+						<a href="https://www.canyoncreeksoftware.com/scheduler/ut/provo/">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/odd-even-calendar.svg" alt="" />
+							<span>SEP Conference Online Scheduler</span>
+						</a>
+					</li>
 				</ul>
 				<a href="https://www.peachjar.com/index.php?region=150121&a=28&b=138"><img src="https://amelia.provo.edu/wp-content/uploads/2019/05/button-orange-eflyers_202x46.png" alt="Link to PeachJar Fliers"></a>
 				<a href="https://safeut.med.utah.edu/"><img src="https://amelia.provo.edu/wp-content/uploads/2019/03/safeUTcrisisline.jpg" alt="Link to SafeUT information"></a>
