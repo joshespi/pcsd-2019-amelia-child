44f11f6731996897108ba6305c1148f634213970
Josh Espinoza <joshespi@gmial.com>
2020-02-26 07:42:05 -0700

    updated Title I info as requested

--- amelia-child/mega-menu-dropdowns/titlei.php
+++ amelia-child/mega-menu-dropdowns/titlei.php
@@ -3,27 +3,21 @@
 						<ul>
 								<li class="pdf"><a href="https://docs.google.com/forms/d/e/1FAIpQLSfCe48I8zIDfz6obN6UIXxZ0OccCEeSiCmsyCwDNIwdnqQd-A/viewform?embedded=true">Title I Parent Info Questionnaire</a></li>
 								<li class="int"><a href="https://amelia.provo.edu/title-i/what-are-the-goals-of-title-i/">What are the Goals of Title I?</a></li>
-								<li class="pdf"><a href="https://amelia.provo.edu/wp-content/uploads/2018/11/Amelia-Earhart-FY19-School-Improvment-Plan.pdf">Title I School Improvement Plan</a></li>
+								
 								<li class="ext"><a href="https://provo.edu/title-i/">District Title I</a></li>
 								
 						</ul>
 						<p>Title I Coordinator: Jonathan Hoehne (JonathanH@provo.edu)</p>
 				</div>
 				<div class="dropDownColumn">
-					<h3><img src="https://amelia.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/procedures1.svg" alt="" />Parent Involvement Policy</h3>
+					<h3><img src="https://amelia.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />School Improvement Plan</h3>
 						<ul>
-							<li class="pdf"><a href="https://amelia.provo.edu/wp-content/uploads/2018/11/2017_18_ParentInvolvementPolicy.pdf">Parent Involvement Policy (English)</a></li>
-							<li class="pdf"><a href="https://amelia.provo.edu/wp-content/uploads/2018/11/2017_18_ParentInvolvementPolicySpanish.pdf">Parent Involvement Policy (Spanish)</a></li>
+							<li class="pdf"><a href="https://amelia.provo.edu/wp-content/uploads/2018/11/Amelia-Earhart-FY19-School-Improvment-Plan.pdf">Title I School Improvement Plan</a></li>
 						</ul>
 					
 				</div>
 				<div class="dropDownColumn">
-					<h3><img src="https://amelia.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Parent-School Compact</h3>
-						<ul>
-							<li class="pdf"><a href="https://amelia.provo.edu/wp-content/uploads/2018/11/2017_ParentSchoolCompact.pdf">Parent-School Compact (English)</a></li>
-							<li class="pdf"><a href="https://amelia.provo.edu/wp-content/uploads/2018/11/2017ParentSchoolCompactSpanish.pdf">Parent-School Compact (Spanish)</a></li>
-						</ul>
-						<h3><img src="https://amelia.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Additional Resources</h3>
+					<h3><img src="https://amelia.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Additional Resources</h3>
 						<ul>
 							<li><a href="https://amelia.provo.edu/wp-content/uploads/2020/01/AE_SchoolReportCard2_FY19.pdf">School Report Card</a></li>
 						</ul>
